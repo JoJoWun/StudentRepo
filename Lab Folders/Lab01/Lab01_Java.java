b1ba5c7ada37910f6f547f23ee77012f17aa6e9b
JoJoWun <159732457+JoJoWun@users.noreply.github.com>
2025-01-23 17:08:56 +0000

    Update Lab01_Java.java
    Lab quiz/task

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-//Edit made by Student- X
+//Edit made by Jordan Oni 230077780
 
   //Last line print('Good bye')
 }
